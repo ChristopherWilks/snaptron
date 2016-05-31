eecaa8f7834b752aa56d8b3c0493d4a6243f7808
Chris Wilks <broadsword@gmail.com>
2016-05-31 18:15:24 -0400

    improved PORT handling for test environment building

--- make_test_area.sh
+++ make_test_area.sh
@@ -10,9 +10,7 @@ rsync -av expected_ucsc_url $1/
 rsync -av expected_ucsc_ids_url $1/
 rsync -av expected_ucsc_format $1/
 rsync -av expected_ucsc_ids_format $1/
+cat ./tests.sh | perl -ne 'chomp; $s=$_; $s=~s/export PORT=1555/export PORT=1300/; print "$s\n";' > $1/tests.sh
 export PATH=$PATH:/data2/kent_tools
 ln -s /data2/gigatron2 $1/data
-echo '!/bin/bash' > $1/tests.sh
-echo "export PORT=${PORT}" >> $1/tests.sh
-tail -71 tests.sh >> $1/tests.sh
 chmod a+x $1/tests.sh
