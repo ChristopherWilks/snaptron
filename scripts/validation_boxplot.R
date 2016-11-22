1be26d6f28ed52a7c4ec00ab5509a99ee4748647
Chris Wilks <broadsword@gmail.com>
2016-11-21 20:21:16 -0500

    adding R script for validation boxplot

---
+++ scripts/validation_boxplot.R
@@ -0,0 +1,11 @@
+infile <- file("stdin")
+m<-read.delim(infile)
+mx<-max(subset(m,group == 'non_validated', select=c(shared_sample_counts)))
+m$group <- factor(m$group,labels=c('Did not validate', 'Validated'))
+
+pdf("./shared_sample_counts.pdf")
+boxplot(m$shared_sample_counts ~ m$group, ylab="Shared-sample count")
+tg <- wilcox.test(m$shared_sample_counts ~ m$group, alternative="less")
+tgp <- paste(c("Wilcox pval",round(tg$p.value,digits=4)), collapse=" ")
+text(1.45, mx+10, tgp)
+dev.off()
