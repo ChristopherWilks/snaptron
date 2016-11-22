87f848993d3ef0b64b82e1253c2c831a54fd7645
Chris Wilks <broadsword@gmail.com>
2016-11-21 20:21:52 -0500

    modified to produce shared-sample ready input files

--- client/bed2snaptron.py
+++ client/bed2snaptron.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python2.7
 import sys
 
-seen = set()
-sys.stdout.write("region\twithin\texact\tthresholds\tgroup\n")
-#assumes input is in start coordinate order sorted
+seen = {}
+sys.stdout.write("region\teither\tthresholds\tgroup\n")
+#assumes input is in start coordinate sorted order
 for line in sys.stdin:
     fields = line.rstrip().split('\t')
-    (chrom,start,end,group) = fields[:4]
-    within = '2'
-    (coord1,coord2)=(end,end)
+    #BED format "name" here is genename
+    #score not used
+    #assumes exon coordinates
+    (chrom,start,end,group,score,strand) = fields[:6]
     if group in seen:
-        within = '1'
-        (coord1,coord2)=(start,start)
-    seen.add(group)
-    sys.stdout.write("%s:%s-%s\t%s\t%s\t%s\t%s\n" %(chrom,coord1,coord2,within,"","",group))
+        seen[group]+=1
+        group = group.replace(" ","_%d " % (seen[group]))
+    else:
+        seen[group]=1
+    #BED offset of 0 to snaptron 1, so we can just use the starts as they are
+    sys.stdout.write("%s:%s-%s\t%d\t%s\t%s\n" %(chrom,start,start,2,"strand=%s&samples_count>=1" % (strand),group))
+    sys.stdout.write("%s:%d-%d\t%d\t%s\t%s\n" %(chrom,int(end)+1,int(end)+1,1,"strand=%s&samples_count>=1" % (strand),group))
