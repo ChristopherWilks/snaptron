8a43adc6063608289811366b007c8c7a8fd002bd
Chris Wilks <broadsword@gmail.com>
2016-11-30 14:36:52 -0500

    added instance setup script; organzing

---
+++ setup_configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln instances/snapconf.py.${1} ./snapconf.py
+ln instances/test_snaptron.py.${1} ./test_snaptron.py
+ln -s instances/tests.sh.${1} ./tests.sh
+ln -s instances/test_s2i_ids_15.snaptron_ids.${1} ./test_s2i_ids_15.snaptron_ids
+ln -s instances/test_annot_full.${1} ./test_annot_full
