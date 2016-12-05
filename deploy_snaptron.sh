2e7a4b7f276b1f8a9868028d52ef5d6100703392
Chris Wilks <broadsword@gmail.com>
2016-12-05 11:52:45 -0500

    added virtualenv dependency to comment

--- deploy_snaptron.sh
+++ deploy_snaptron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Deploys snaptron for whatever data source label was passed in (srav1,srav2,tcga,gtex)
 
-#Tabix and Sqlite3 need to be compiled and already in the PATH *before*
+#Tabix, Sqlite3, and virtualenv need to be compiled and already in the PATH *before*
 #running this script
 
 #setup python for Snaptron
@@ -26,9 +26,9 @@ tabix -s 2 -b 3 -e 4 junctions.bgz
 tabix -s 1 -b 4 -e 5 all_transcripts.gtf.bgz
 
 #creation of sqlite junction db
-scripts/build_sqlite_junction_db.sh ${1}_junc junctions.bgz
+scripts/build_sqlite_junction_db.sh junctions junctions.bgz
 #creation of sample2junction of sqlite by_sample_id db
-scripts/build_sqlite_sample_mapping_db.sh ${1}_sample_ids junctions.bgz
+scripts/build_sqlite_sample_mapping_db.sh sample2junction junctions.bgz
 
 #run lucene indexer on metadata
 cat samples.tsv | perl -ne 'chomp; $n++; @f=split(/\t/,$_); foreach $idx (0 .. (scalar @f)-1) { $type="t"; $type="n" if($f[$idx] eq "NA"); if($f[$idx]=~/^-?\d+?\.?\d+$/) { $type="f"; } $type="i" if($f[$idx]=~/^-?\d+$/); $counts{$idx}->{$type}++; } END { foreach $idx (sort { $a<=>$b} keys %counts) { print "$idx"; $counts{$idx}->{"n"}=-$counts{$idx}->{"n"} if(defined($counts{$idx}->{"n"})); $counts{$idx}->{"i"}=-$counts{$idx}->{"i"} if(defined($counts{$idx}->{"f"}) && defined($counts{$idx}->{"i"}) > 0); foreach $t (sort {$counts{$idx}->{$b}<=>$counts{$idx}->{$a}} keys %{$counts{$idx}}) { print "\t$t,".$counts{$idx}->{$t}; } print "\n";}}' > samples.tsv.type_inference
