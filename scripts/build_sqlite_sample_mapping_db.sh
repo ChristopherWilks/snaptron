be2c642a42a92cb240bac385550dfe9038f2c7c9
Chris Wilks <cwilks3@jhu.edu@compute0108.cm.cluster>
2016-12-07 14:38:57 -0500

    added -T to sort to use local directory

--- scripts/build_sqlite_sample_mapping_db.sh
+++ scripts/build_sqlite_sample_mapping_db.sh
@@ -5,6 +5,6 @@ sqlite3 ${1}.sqlite "CREATE TABLE by_sample_id (sample_id INTEGER NOT NULL,snapt
 
 #create the sample2junction ID mapping and populate the sqlite3 table
 mkfifo ./import
-zcat ${2} | cut -f1,12 | perl -ne 'chomp; ($tid,$sid)=split(/\t/,$_); @sids=split(/,/,$sid); foreach $s (@sids) { print "$s\t$tid\n";}' | sort -k1,1 -n | perl -ne 'chomp; $s=$_; ($sid,$tid)=split(/\t/,$s); if(defined($pid) && $pid != $sid) { print "$pid\t"; foreach $s (sort keys %h) {print "$s,";} print "\n"; %h=();} $pid=$sid; $h{$tid}=1; END { if(defined($pid)) { print "$pid\t"; foreach $s (sort keys %h) {print "$s,";} print "\n";}}' > ./import &
+zcat ${2} | cut -f1,12 | perl -ne 'chomp; ($tid,$sid)=split(/\t/,$_); @sids=split(/,/,$sid); foreach $s (@sids) { print "$s\t$tid\n";}' | sort -T ./ -k1,1 -n | perl -ne 'chomp; $s=$_; ($sid,$tid)=split(/\t/,$s); if(defined($pid) && $pid != $sid) { print "$pid\t"; foreach $s (sort keys %h) {print "$s,";} print "\n"; %h=();} $pid=$sid; $h{$tid}=1; END { if(defined($pid)) { print "$pid\t"; foreach $s (sort keys %h) {print "$s,";} print "\n";}}' > ./import &
 sqlite3 ${1}.sqlite -cmd '.separator "\t"' ".import ./import by_sample_id"
 rm ./import
