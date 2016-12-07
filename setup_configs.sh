218a9e6d2e7e4497659bd246c127c777fce1f8ec
Chris Wilks <cwilks3@jhu.edu@compute0001.cm.cluster>
2016-12-07 17:11:44 -0500

    added if-then for srav*

--- setup_configs.sh
+++ setup_configs.sh
@@ -2,10 +2,13 @@
 
 ln -fs ./snaptron_server ./${1}_snaptron_server
 ln -f instances/snapconf.py.${1} ./snapconf.py
-ln -f instances/test_snaptron.py.${1} ./test_snaptron.py
-ln -fs instances/tests.sh.${1} ./tests.sh
-ln -fs instances/test_s2i_ids_15.snaptron_ids.${1} ./test_s2i_ids_15.snaptron_ids
-ln -fs instances/test_annot_full.${1} ./test_annot_full
-ln -fs lucene_indexer.py.sra instances/lucene_indexer.py.srav1
-ln -fs lucene_indexer.py.sra instances/lucene_indexer.py.srav2
+if [ $1 = "srav1" ] || [ $1 = "srav2" ] ; then
+	ln -f instances/test_snaptron.py.${1} ./test_snaptron.py
+	ln -fs instances/tests.sh.${1} ./tests.sh
+	ln -fs instances/test_s2i_ids_15.snaptron_ids.${1} ./test_s2i_ids_15.snaptron_ids
+	ln -fs instances/test_annot_full.${1} ./test_annot_full
+	ln -fs lucene_indexer.py.sra instances/lucene_indexer.py.srav1
+	ln -fs lucene_indexer.py.sra instances/lucene_indexer.py.srav2
+fi
+ln -fs lucene_indexer.py.gtex instances/lucene_indexer.py.tcga
 ln -fs instances/lucene_indexer.py.${1} ./lucene_indexer.py
