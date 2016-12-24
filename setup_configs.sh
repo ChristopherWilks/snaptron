e04146c6211a2dbc65b99838eb55f0ff2850ac27
Chris Wilks <broadsword@gmail.com>
2016-12-23 19:07:58 -0500

    brought UCSC format/url tests to srav2

--- setup_configs.sh
+++ setup_configs.sh
@@ -7,6 +7,7 @@ if [ $1 = "srav1" ] || [ $1 = "srav2" ] ; then
 	ln -fs instances/tests.sh.${1} ./tests.sh
 	ln -fs instances/test_s2i_ids_15.snaptron_ids.${1} ./test_s2i_ids_15.snaptron_ids
 	ln -fs instances/test_annot_full.${1} ./test_annot_full
+	ls instances/*ucsc*.${1} | perl -ne 'chomp; $s=$_; $s1=$s; $s1=~s/\.srav.$//; $s1=~s/instances\///; `ln -fs $s $s1`;'
 fi
 if [ $1 = "srav2" ] ; then
 	ln -fs instances/test_s2i_ids_3.snaptron_ids.${1} ./test_s2i_ids_3.snaptron_ids
