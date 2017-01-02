3e417e4662787275aa6b868d5d3e888403c5910e
Chris Wilks <broadsword@gmail.com>
2017-01-02 14:47:39 -0500

    remove wc to just dump results

--- scripts/server/test_snaptron_wsi.sh
+++ scripts/server/test_snaptron_wsi.sh
@@ -4,5 +4,5 @@ set -x
 declare -a insts=( "srav1" "srav2" "gtex" "tcga" )
 for x in "${insts[@]}"
 do
-	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=CD99&rfilter=samples_count:20" | grep -i "${x}:I" | wc -l
+	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=CD99&rfilter=samples_count:20" | grep -i "${x}:I"
 done
