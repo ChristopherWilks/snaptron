586d3d1b5c25b28cfdc4dc5db85bf5d9e81e3440
Chris Wilks <broadsword@gmail.com>
2017-02-15 14:51:52 -0500

    added CC BY-NC 4.0 license file and header

--- snample.py
+++ snample.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python2.7
+
+# Copyright 2016, Christopher Wilks <broadsword@gmail.com>
+#
+# This file is part of Snaptron.
+#
+# Snaptron is free software: you can redistribute it and/or modify
+# it under the terms of the 
+# Creative Commons Attribution-NonCommercial 4.0 
+# International Public License ("CC BY-NC 4.0").
+#
+# Snaptron is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# CC BY-NC 4.0 license for more details.
+#
+# You should have received a copy of the CC BY-NC 4.0 license
+# along with Snaptron.  If not, see 
+# <https://creativecommons.org/licenses/by-nc/4.0/legalcode>.
+
 '''
 Takes sample and sample ID queries.
 Returns list of sample metadata and/or intron IDs mapping to the sample queries.
--- snannotation.py
+++ snannotation.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python2.7
+
+# Copyright 2016, Christopher Wilks <broadsword@gmail.com>
+#
+# This file is part of Snaptron.
+#
+# Snaptron is free software: you can redistribute it and/or modify
+# it under the terms of the 
+# Creative Commons Attribution-NonCommercial 4.0 
+# International Public License ("CC BY-NC 4.0").
+#
+# Snaptron is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# CC BY-NC 4.0 license for more details.
+#
+# You should have received a copy of the CC BY-NC 4.0 license
+# along with Snaptron.  If not, see 
+# <https://creativecommons.org/licenses/by-nc/4.0/legalcode>.
+
 '''
 Similar to the main Snaptron service, takes coordinate range or gene name
 and returns list of transcript exons
