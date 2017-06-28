6f8ce6332bbbc3aa9c6722d994f2f9ef7c8c7c14
Chris Wilks <broadsword@gmail.com>
2017-06-28 02:24:56 -0400

    generic deploy script to do full setup of non-human/original Snaptron

---
+++ deploy_snaptron_generic.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#Deploys snaptron for a new compilation (not one of the original 4: srav1,srav2,gtex,tcga)
+
+#Tabix >= 1.2.1 and Sqlite3 >= 3.11.0 need to be compiled and already in the PATH *before*
+#running this script
+
+#required arguments (example of a a run on the "supermouse" compilation below):
+#1) name of compilation
+#2) path to Rail's cross-sample junction call file (junctions.tsv.gz)
+#3) path to bowtie index of source genome
+#4) path to directory of already downloaded annotation GTFs from UCSC's table browser
+#5) new data source ID (check current set of compilations to know which to use)
+#6) path to TSV file of sample metadata information with first column as rail_id (integer ID)
+
+#7) [optional] argument to use the uncompressed version of Tabix (pass a 1)
+
+#./deploy_snaptron_generic.sh supermouse ./junctions.tsv.gz /data3/indexes/mm10/genome ./annotations 6 ./samples.tsv 1
+
+#our modified version of bgzip must also be present in the PATH
+#before any other bgzip binaries
+#get it here:
+#http://snaptron.cs.jhu.edu/data/htslib-1.2.1_nocomp.tar.gz
+
+echo "+++Setting up Python for Snaptron for ${1} compilation"
+#setup python for Snaptron
+#from https://virtualenv.pypa.io/en/stable/installation/
+curl -k -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.2.tar.gz
+tar xvfz virtualenv-13.1.2.tar.gz
+cd virtualenv-13.1.2
+python virtualenv.py ../python
+cd ..
+source ./python/bin/activate
+pip install -r requirements.txt
+
+echo "+++Setting up PyLucene and dependencies (this requires sudo)"
+#this requires additional packages at the system level
+./install_pylucene.sh
+
+echo "+++Linking config files for ${1} compilation"
+#link config file(s)
+#YOU WILL NEED TO TAILOR THIS FILE TO YOUR COMPILATION's specific settings
+ln -fs ./snaptron_server ./${1}_snaptron_server
+rsync -av instances/snapconf.py.srav2 instances/snapconf.py.${1}
+ln -f instances/snapconf.py.${1} ./snapconf.py
+
+#setup data
+mkdir data
+rsync -av $6 data/samples.tsv
+cd data
+../scripts/setup_generic_snaptron_instance.sh $2 $3 $4 $5 $7
+#echo "+++Downloading snaptron data, this make take a while..."
+wget http://snaptron.cs.jhu.edu/data/srav2/all_transcripts.gtf.bgz
+wget http://snaptron.cs.jhu.edu/data/srav2/refseq_transcripts_by_hgvs.tsv
+wget http://snaptron.cs.jhu.edu/data/srav2/ucsc_known_canonical_transcript.tsv
+
+echo "+++Creating Tabix index on transcripts file"
+#creation of Tabix index of all_transcripts
+tabix -s 1 -b 4 -e 5 all_transcripts.gtf.bgz
+
+cd ../
+ln -s data/lucene_indexed_numeric_types.tsv
--- scripts/setup_generic_snaptron_instance.sh
+++ scripts/setup_generic_snaptron_instance.sh
@@ -9,9 +9,12 @@
 #and metadata tsv should be named "samples.tsv" and be in the current directory
 
 #args:
-#1) path to bowtie index of origin genome
-#2) path to directory of already downloaded annotation GTFs from UCSC's table browser
-#3) new data source ID (check current set of compilations to know which to use)
+#1) path to Rail's cross-sample junction call file (junctions.tsv.gz)
+#2) path to bowtie index of origin genome
+#3) path to directory of already downloaded annotation GTFs from UCSC's table browser
+#4) new data source ID (check current set of compilations to know which to use)
+
+#5) [optional] argument to use the uncompressed version of Tabix (pass a 1)
 
 #get the snaptron checkout root dir (assuming the script is called from there)
 scripts=`perl -e '@f=split(/\//,"'${0}'"); pop(@f); print "".join("/",@f)."\n";'`
@@ -19,13 +22,13 @@ root=$scripts/../
 source $root/python/bin/activate
 
 #this takes in as --input-file the "raw" cross-sample junctions output from rail
-python $scripts/convert_rail_raw_cross_sample_junctions.py --data-src $3 --input-file junctions.tsv.gz --bowtie-idx $1 | bgzip > junctions.tsv.snap.bgz
+python $scripts/convert_rail_raw_cross_sample_junctions.py --data-src $4 --input-file $1 --bowtie-idx $2 | bgzip > junctions.tsv.snap.bgz
 
 #To get the raw annotation GTFs:
 #1) select each of the various annotations you want from the UCSC genome browser table browser interface 
 #2) choose GTF as output format)
 #3) then save them all to the path for --annotations specified below
-python $root/annotations/rip_annotated_junctions.py --extract-script-dir $root --annotations $2 | gzip > ripped_annotations.tsv.gz
+python $root/annotations/rip_annotated_junctions.py --extract-script-dir $root --annotations $3 | gzip > ripped_annotations.tsv.gz
 
 #annotate and build final snaptron
 zcat junctions.tsv.snap.bgz | cut -f2,3,4,6,8,9,12- | pypy $root/annotations/process_introns.py --annotations ripped_annotations.tsv.gz | bgzip > junctions.bgz
@@ -35,15 +38,19 @@ rm junctions.tsv.snap.bgz
 tabix -s 2 -b 3 -e 4 junctions.bgz
 
 #build uncompressed version of BGZipped Tabix index for speed
-zcat junctions.bgz | bgzip_uncompressed > junctions_uncompressed.bgz
+if [ -z ${5+v} ]; then
+        echo "sticking with compressed junctions file"
+        ln -s junctions.bgz junctions_uncompressed.bgz
+else
+#The compress_level=0 modified bgzf must be in your path before any standard bgzf binaries
+        echo "creating uncompressed junctions file"
+        zcat junctions.bgz | bgzip > junctions_uncompressed.bgz
+fi
 tabix -s 2 -b 3 -e 4 junctions_uncompressed.bgz
 
 #build junctions.sqlite
 $scripts/build_sqlite_junction_db.sh junctions junctions.bgz
 
-#build Lucene metadata indices
+#build Lucene metadata indices (assumes samples.tsv is present)
 cat samples.tsv | perl $scripts/infer_sample_metadata_field_types.pl > samples.tsv.inferred
 cat samples.tsv | python $root/lucene_indexer.py samples.tsv.inferred > lucene.indexer.run 2>&1 &
-
-#TODO, need to move this file into the root of the snaptron compilation server
-rsync -av lucene_indexed_numeric_types.tsv $root/
