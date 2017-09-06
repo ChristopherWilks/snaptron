72f1b84b017cd84eb29f28730347babb521cec41
Chris Wilks <broadsword@gmail.com>
2017-09-06 18:13:17 -0400

    added mouseling, supermouse, and encode1159 to list of compilations to start/test

--- scripts/server/start_all_snaptron_servers.sh
+++ scripts/server/start_all_snaptron_servers.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -x
 
-#declare -a insts=( "srav1" "srav2" "gtex" "tcga" "test" )
-declare -a insts=( "test" )
+declare -a insts=( "srav1" "srav2" "gtex" "tcga" "mouseling" "supermouse" "encode1159" )
 cd /data/gigatron/
 for x in "${insts[@]}"
 do
-	screen -d -m ./run_snaptron_server.sh ${x}
+	screen -S ${x} -d -m ./run_snaptron_server.sh ${x}
 done
--- scripts/server/test_snaptron_wsi.sh
+++ scripts/server/test_snaptron_wsi.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -x
 
-declare -a insts=( "srav1" "srav2" "gtex" "tcga" )
+declare -a insts=( "srav1" "srav2" "gtex" "tcga")
 for x in "${insts[@]}"
 do
 	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=CD99&rfilter=samples_count:20" | grep -i "${x}:I"
 done
+
+
+declare -a insts=( "encode1159" "supermouse" )
+for x in "${insts[@]}"
+do
+	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=chr1:1-20854861&rfilter=samples_count:200" | grep -i "${x}:I"
+done
+
+curl "http://snaptron.cs.jhu.edu/mouseling/snaptron?regions=chr1:1-20854861&rfilter=samples_count:50" | grep -i "mouseling:I"
