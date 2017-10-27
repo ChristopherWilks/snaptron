4dd3ae2f66a92c2322cff6ac3b6d26494f500493
Chris Wilks <broadsword@gmail.com>
2017-10-27 17:45:38 -0400

    updated smoke testing script with gene expression query

--- scripts/server/test_snaptron_wsi.sh
+++ scripts/server/test_snaptron_wsi.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-set -x
+#set -x
 
 declare -a insts=( "srav1" "srav2" "gtex" "tcga")
 for x in "${insts[@]}"
 do
-	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=CD99&rfilter=samples_count:20" | grep -i "${x}:I"
+	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=CD99&rfilter=samples_count:20" 2>/dev/null| grep -i "${x}:I" | head -1 | cut -f 1-10
+	#test ALK region
+	curl "http://snaptron.cs.jhu.edu/${x}/genes?regions=chr2:29899597-29907199" 2>/dev/null| grep -i "${x}:G" | head -1 | cut -f 1-10
 done
 
 
 declare -a insts=( "encode1159" "supermouse" )
 for x in "${insts[@]}"
 do
-	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=chr1:1-20854861&rfilter=samples_count:200" | grep -i "${x}:I"
+	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=chr1:1-20854861&rfilter=samples_count:200" 2>/dev/null| grep -i "${x}:I" | head -1 | cut -f 1-10
+	#test ALK region
+	curl "http://snaptron.cs.jhu.edu/${x}/genes?regions=chr2:29899597-29907199" 2>/dev/null| grep -i "${x}:G" | head -1 | cut -f 1-10
 done
 
-curl "http://snaptron.cs.jhu.edu/mouseling/snaptron?regions=chr1:1-20854861&rfilter=samples_count:50" | grep -i "mouseling:I"
+#curl "http://snaptron.cs.jhu.edu/mouseling/snaptron?regions=chr1:1-20854861&rfilter=samples_count:50" 2>/dev/null| grep -i "mouseling:I" | head -1
