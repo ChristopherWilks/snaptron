b0611cfd4af15074fef7eb147a1ab6cd56a4285b
Chris Wilks <broadsword@gmail.com>
2017-11-14 16:22:36 -0500

    added base endpoint to wsi test script

--- scripts/server/test_snaptron_wsi.sh
+++ scripts/server/test_snaptron_wsi.sh
@@ -4,18 +4,26 @@
 declare -a insts=( "srav1" "srav2" "gtex" "tcga")
 for x in "${insts[@]}"
 do
+	echo "testing $x junctions"
 	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=CD99&rfilter=samples_count:20" 2>/dev/null| grep -i "${x}:I" | head -1 | cut -f 1-10
 	#test ALK region
+	echo "testing $x genes"
 	curl "http://snaptron.cs.jhu.edu/${x}/genes?regions=chr2:29899597-29907199" 2>/dev/null| grep -i "${x}:G" | head -1 | cut -f 1-10
+	echo "testing $x bases"
+	curl "http://snaptron.cs.jhu.edu/${x}/bases?regions=chr2:29899597-29907199" 2>/dev/null| grep -i "${x}:B" | head -1 | cut -f 1-10
 done
 
 
 declare -a insts=( "encode1159" "supermouse" )
 for x in "${insts[@]}"
 do
+	echo "testing $x junctions"
 	curl "http://snaptron.cs.jhu.edu/${x}/snaptron?regions=chr1:1-20854861&rfilter=samples_count:200" 2>/dev/null| grep -i "${x}:I" | head -1 | cut -f 1-10
 	#test ALK region
+	echo "testing $x genes"
 	curl "http://snaptron.cs.jhu.edu/${x}/genes?regions=chr2:29899597-29907199" 2>/dev/null| grep -i "${x}:G" | head -1 | cut -f 1-10
+	echo "testing $x bases"
+	curl "http://snaptron.cs.jhu.edu/${x}/bases?regions=chr2:29899597-29907199" 2>/dev/null| grep -i "${x}:B" | head -1 | cut -f 1-10
 done
 
 #curl "http://snaptron.cs.jhu.edu/mouseling/snaptron?regions=chr1:1-20854861&rfilter=samples_count:50" 2>/dev/null| grep -i "mouseling:I" | head -1
