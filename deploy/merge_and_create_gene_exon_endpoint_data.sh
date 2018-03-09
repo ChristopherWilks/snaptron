cb22dfda647bd5d09939a2dcbca8d956805c2b73
ChristopherWilks <broadsword@gmail.com>
2018-03-09 11:22:50 -0500

    gene/exon index creation supports fixing up for artificial splits

--- deploy/merge_and_create_gene_exon_endpoint_data.sh
+++ deploy/merge_and_create_gene_exon_endpoint_data.sh
@@ -6,14 +6,19 @@
 #3) full path to file containing sample metadata from Rail for this compilation (/data/snaptron_data/<compilation>/samples.tsv)
 #4) source compilation ID (e.g. 2 for SRAv2)
 #5) number of splits (usually 10 or 14)
-
-SUFFIX1='.0.snapout.'
-SUFFIX2='.tsv'
+#6) [optional] path to list of original disjoint exons from recount (e.g. recount_hg38_gencodev25_disjoint_exons.tsv)
+#if this last is passed in, the fixup script will be run merge pre-run, artificially split exons (1000bp each)
 
 #get this script's path
 p=`perl -e '@f=split("/","'$0'"); pop(@f); print "".join("/",@f)."\n";'`
 echo $p
 
-perl -e 'for $a (("exons","genes")) { $i2=0; `head -1 /dataB/0.'${1}'.$a > ./'${1}'.all.$a`; for $i (66..(66+'${5}'-1)) { $i1=chr($i); `cat /data$i1/*.'${1}'.$a | grep -v start >> ./'${1}'.all.$a`; $i2++;}}'
+perl -e 'for $a (("exons","genes")) { $i2=0; `head -1 /dataB/'${1}'.0.snapout.$a.tsv > ./'${1}'.all.$a`; for $i (66..(66+'${5}'-1)) { $i1=chr($i); `cat /data$i1/'${1}'.$i2.snapout.$a.tsv | grep -v start >> ./'${1}'.all.$a`; $i2++;}}'
+
+if [[ $6 ]]; then
+	#assumes snaptron server checkout also has the client checked out
+	python ${p}/../client/scripts/fixup_gene_exon_bulk_base_intervals.py $6 ${1}.all.exons > ${1}.all.exons.fixed
+	mv ${1}.all.exons.fixed ${1}.all.exons
+fi
 
 ${p}/../deploy/decorate_genes_exons.sh ./${1}.all ${2} ${1} ${3} ${4}
