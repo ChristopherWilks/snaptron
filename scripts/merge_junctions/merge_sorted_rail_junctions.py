a7f7b8443853e1a1427bb81e572f0d59d318a70a
ChristopherWilks <broadsword@gmail.com>
2018-03-16 20:17:27 -0400

    multi rail output merge script now 1) dumps out sample id2SRR map 2) skips duplicate sample counts

--- scripts/merge_junctions/merge_sorted_rail_junctions.py
+++ scripts/merge_junctions/merge_sorted_rail_junctions.py
@@ -6,27 +6,69 @@ import sys
 import re
 import gzip
 
+
 file_index_patt = re.compile(r'(\d\d)')
 def open_files(files):
     fh_map = {}
     for file_ in files:
-        #fh = gzip.open(file_,"rb")
         fh = open(file_,"rb")
         m = file_index_patt.search(file_)
         fidx = m.group(1) 
         fh_map[fidx]=[0,fh]
     return fh_map
 
-def append_sample_counts(sample_id_offset,psamples,pcounts,samples,counts):
-    return(psamples+",".join([str(int(sid)+sample_id_offset) for sid in samples.split(',')]),pcounts+counts) 
+def append_sample_counts(sample_id_offset,psamples,pcounts,samples,counts,fID,fID2sample,printed):
+    sample_ids = []
+    new_counts = []
+    counts = counts.split(',')
+    for (i,sid) in enumerate(samples.split(',')):
+        k = "%s.%s" % (fID,sid)
+        if k not in fID2sample:
+            continue
+        (srp,srr) = fID2sample[k]
+        new_id = int(sid) + sample_id_offset
+        sample_ids.append(str(new_id))
+        new_counts.append(counts[i])
+        if k not in printed:
+            printed.add(k)
+            sys.stderr.write("%s\t%d\t%s\n" % (k,new_id,srr)) 
+    return(psamples+",".join(sample_ids),pcounts+",".join(new_counts)) 
+   
+def load_samples(samples_file):
+    fID2sample = {}
+    fIDcounts = {}
+    srrs = set()
+    count = 0
+    pfID = None
+    with open(samples_file,"rb") as fin:
+        for line in fin:
+            #fID ~ 01, lID ~ 5 (starts at 0), study_sample ~ SRP-SRR
+            (fID,lID,study_sample) = line.rstrip().split('\t')
+            if pfID is not None and pfID != fID:
+                fIDcounts[fID]=count
+                count = 0
+            (study,sample) = study_sample.split('-')
+            pfID = fID
+            #skip duplicates
+            if sample in srrs:
+                continue
+            srrs.add(sample)
+            fID2sample['%s.%s' % (fID,lID)]=[study,sample]
+            count += 1 
+        if pfID is not None:
+            fIDcounts[fID]=count
+    return (fID2sample, fIDcounts)  
     
+ 
 def main():
     samples_per_file = int(sys.argv[1])
     sorted_coords_file = sys.argv[2]
-    files = sys.argv[3:]
+    (fID2sample, fIDcounts) = load_samples(sys.argv[3])
+    files = sys.argv[4:]
     fhs = open_files(files)
     cfin = open(sorted_coords_file,"rb")
     (pchrm,pstrand,pstart,pend,psamples,pcounts)=(None,None,None,None,"","")
+    printed = set()
     for line in cfin:
         line = line.rstrip()
         (fID_,chrm,start,end)=line.split('\t')
@@ -40,16 +82,14 @@ def main():
         if pchrm != chrm or pstart != start or pend != end:
             if pchrm is not None:
                 sys.stdout.write("\t".join([pchrm,pstrand,pstart,pend,psamples,pcounts])+"\n")
-            (psamples,pcounts)=append_sample_counts((int(fID)-1)*samples_per_file,"","",samples,counts)
+            (psamples,pcounts)=append_sample_counts((int(fID)-1)*samples_per_file,"","",samples,counts,fID,fID2sample,printed)
             (pchrm,pstrand,pstart,pend)=(chrm,strand,start,end)
         else:
             #offset the sample_per_file offset calculation by 1 so we start at 0
-            (psamples,pcounts)=append_sample_counts((int(fID)-1)*samples_per_file,psamples+",",pcounts+",",samples,counts)
+            (psamples,pcounts)=append_sample_counts((int(fID)-1)*samples_per_file,psamples+",",pcounts+",",samples,counts,fID,fID2sample,printed)
     if pchrm is not None:
         sys.stdout.write("\t".join([pchrm,pstrand,pstart,pend,psamples,pcounts])+"\n")
     cfin.close()
-    for (fidx,f) in fhs.iteritems():
-        f.close()
 
 if __name__ == '__main__':
     main()
