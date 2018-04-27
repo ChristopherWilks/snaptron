d9c196962b225f39aa9bfc278f785033eed867ab
Chris Wilks <broadsword@gmail.com>
2018-04-27 11:31:31 -0400

    added recount/R rounding, fixed argument document defaults for normalizing

--- scripts/recount_expression/normalize_expression.py
+++ scripts/recount_expression/normalize_expression.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 import gzip
+import clsnaputil
 
 #assumes you already have the AUCs
 #pulled out using:
@@ -28,20 +29,20 @@ def normalize_counts(args,aucs):
         for line in fin:
             fields = line.rstrip().split('\t')
             gene_id = fields[0]
-            if gene_id == 'gene_id':
+            if gene_id == 'gene_id' or gene_id == 'Group':
                 sys.stdout.write(line)
                 header={i:x for (i,x) in enumerate(fields[args.count_start_col:])}
                 continue
-            fields = [(RECOUNT_TARGET * float(x))/float(aucs[header[i]]) for (i,x) in enumerate(fields[args.count_start_col:])]
+            fields = [int(clsnaputil.round_like_R((RECOUNT_TARGET * float(x))/float(aucs[header[i]]),0)) for (i,x) in enumerate(fields[args.count_start_col:])]
             sys.stdout.write(gene_id+"\t"+"\t".join(map(str,fields))+"\n")
 
 def main():
     parser = argparse.ArgumentParser(description='Normalization of raw counts')
     parser.add_argument('--counts-file', metavar='/path/to/counts_file', type=str, default=None, help='path to a TSV file with matrix of counts')
     parser.add_argument('--metadata-file', metavar='/path/to/sample_metadata_file', type=str, default=None, help='path to a TSV file with the Snaptron sample metadata')
-    parser.add_argument('--auc-col', metavar='1', type=int, default=-1, help='which column in the sample metadata contains the AUC, default is the last')
+    parser.add_argument('--auc-col', metavar='-1', type=int, default=-1, help='which column in the sample metadata contains the AUC, default is the last')
     parser.add_argument('--count-start-col', metavar='1', type=int, default=1, help='which column in the raw coverage file start the counts')
-    parser.add_argument('--sample-id-col', metavar='1', type=int, default=0, help='which column in the sample metadata contains the joining ID')
+    parser.add_argument('--sample-id-col', metavar='0', type=int, default=0, help='which column in the sample metadata contains the joining ID')
     args = parser.parse_args()
 
     aucs = load_metadata(args)
