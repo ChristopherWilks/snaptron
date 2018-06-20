625715d8bf9bdcfe6ed420f7d3d981f6ad53c4c7
Chris Wilks <broadsword@gmail.com>
2018-06-20 18:40:09 -0400

    added support for exons/junctions, coords-as-ids, and skipping all 0 rows in normalization script

--- scripts/recount_expression/normalize_expression.py
+++ scripts/recount_expression/normalize_expression.py
@@ -9,6 +9,7 @@ import clsnaputil
 #wiggletools print non_unique_base_coverage.bw.auc AUC non_unique_base_coverage.bw
 
 RECOUNT_TARGET = 40 * 1000000
+SNAPTRON_FORMAT_CHR_COL = 1
 SNAPTRON_FORMAT_COUNT_COL = 11
 
 def load_metadata(args):
@@ -42,7 +43,9 @@ def normalize_counts(args, aucs, sids):
             fields = None
             fields__ = line.rstrip().split('\t')
             if args.snaptron_format:
-                gene_id = fields__[SNAPTRON_FORMAT_COUNT_COL-1].split(':')[0]
+                gene_id = fields__[SNAPTRON_FORMAT_CHR_COL] + ':' + '-'.join(fields__[SNAPTRON_FORMAT_CHR_COL+1:SNAPTRON_FORMAT_CHR_COL+3])
+                if args.id_type == 'gene_id':
+                    gene_id = fields__[SNAPTRON_FORMAT_COUNT_COL-1].split(':')[0]
                 fields = {sid:0 for sid in sids}
                 for sample in fields__[SNAPTRON_FORMAT_COUNT_COL].split(',')[1:]:
                     (sid, count) = sample.split(':')
@@ -56,6 +59,10 @@ def normalize_counts(args, aucs, sids):
                 else:
                     fields = {sids[i]:int(count) for (i,count) in enumerate(fields__[args.count_start_col:])}
             fields = [int(clsnaputil.round_like_R((RECOUNT_TARGET * float(fields[sid]))/float(aucs[sid]),0)) for sid in sids]
+            if args.skip_0_rows:
+                zeros = [1 for x in fields if x == 0]
+                if len(zeros) == len(fields):
+                    continue
             sys.stdout.write(gene_id+"\t"+"\t".join(map(str,fields))+"\n")
 
 def main():
@@ -66,6 +73,8 @@ def main():
     parser.add_argument('--count-start-col', metavar='1', type=int, default=1, help='which column in the raw coverage file start the counts')
     parser.add_argument('--sample-id-col', metavar='0', type=int, default=0, help='which column in the sample metadata contains the joining ID')
     parser.add_argument('--snaptron-format', action='store_const', const=True, default=False, help='if gene raw counts file is coming from the genes Snaptron formatted DB')
+    parser.add_argument('--id-type', metavar='how to format the ID field', type=str, default='gene_id', help='what kind of row ID to use: "gene_id" (default), "coord"; only matters if --snaptron-format is also passed in')
+    parser.add_argument('--skip-0-rows', action='store_const', const=True, default=False, help='if all normalized counts in a row are zeros, drop the row from output')
     args = parser.parse_args()
 
     if args.snaptron_format:
