aec4b9de70033e5decb364e447650f7ee16191db
Chris Wilks <broadsword@gmail.com>
2018-08-15 18:27:48 -0400

    start all instance script now takes full path to root dir of snaptron

--- scripts/server/start_all_snaptrons.sh
+++ scripts/server/start_all_snaptrons.sh
@@ -1,1 +1,5 @@
-cat ~/snaptron_instances.txt | perl -ne 'chomp; $e=$_; $e1="snaptron"; $e1=$e."_snaptron" if($e ne "snaptron"); $e2=$e1."_server"; `screen -d -m -S $e ./run_snaptron_compilation.sh /home/cwilks3\@jhu.edu/gigatron/$e1 $e2`;'
+#!/bin/bash
+
+#Pass in the full path where all the snaptron instances are subdirs
+ROOT_PATH=$1
+cat ~/snaptron_instances.txt | perl -ne 'chomp; $e=$_; $e1="snaptron"; $e1=$e."_snaptron" if($e ne "snaptron"); $e2=$e1."_server"; `screen -d -m -S $e ./run_snaptron_compilation.sh '${ROOT_PATH}'/$e1 $e2`;'
