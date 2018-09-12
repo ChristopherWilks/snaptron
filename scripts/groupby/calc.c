0956d8d1e9bf79d27b7d6495367f4d6d276a0c13
Chris Wilks <broadsword@gmail.com>
2018-09-12 15:32:21 -0400

    both row and column axes now working in calc program

--- scripts/groupby/calc.c
+++ scripts/groupby/calc.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
+
+//follows numpy convention,
+//assuming 2-dim matrix, row operations
+//summarizes across all rows, (#lines == 1, #columns = original input)
+//while column operations summarize across the columns (#lines == #rows, #columns == 1)
+int ROW_AXIS=0;
+int COL_AXIS=1;
 
 typedef struct config_def {
 	int bp_col;
@@ -11,17 +19,28 @@ typedef struct config_def {
 	int base_col_start;
 	int axis;
 	char* op;
+	char* label;
 } config_settings;
 
-int process_line(char* line, char* delim, double* value, config_settings config_def, int print_coord) //double** counts
+int process_line(int first, char* line, char* delim, double** vals, config_settings config_def, int print_coord) //double** vals
 {
 	char* line_copy = strdup(line);
 	char* tok = strtok(line_copy, delim);
 	int i = 0;
 	char* chrm;
 	long start, end;
+	int static_idx = config_def.base_col_start;
+	int* col_idx = &static_idx;
+	if(config_def.axis == ROW_AXIS)
+		col_idx = &i;
 	while(tok != NULL)
 	{
+		if(first)
+		{
+			i++;
+			tok = strtok(NULL,delim);
+			continue;
+		}
 		if(i == config_def.chrm_col)
 			chrm=strdup(tok);
 		if(i == config_def.start_col)
@@ -31,61 +50,130 @@ int process_line(char* line, char* delim, double* value, config_settings config_
 		if(i >= config_def.base_col_start)
 		{
 			if(strcmp(config_def.op, "sum") == 0 || strcmp(config_def.op, "mean") == 0)
-				*value += atof(tok);
-			//(*counts)[i-config_def.base_col_start] += atof(tok);
+				(*vals)[(*col_idx)-config_def.base_col_start] += atof(tok);
 		}
 		i++;
 		tok = strtok(NULL,delim);
 	}
-	if(strcmp(config_def.op, "mean") == 0)
-		*value /= (i - config_def.base_col_start);
-	if(print_coord)
-	{
+	if(!first && print_coord)
 		fprintf(stdout,"%s\t%lu\t%lu",chrm,start,end);
-	}
 	if(line_copy)
 		free(line_copy);
+	if(!first && chrm)
+		free(chrm);
 	if(line)
 		free(line);
-	if(chrm)
-		free(chrm);
 	return i;
 }
+
+//parameters for the program
+static struct option long_opts[] = 
+{
+	//can be 0 (ROW) or 1 (COLUMN)
+	{"axis", required_argument, 0, 'a'},
+	//can be "sum" or "mean"
+	{"op", required_argument, 0, 'o'},
+	//short, descriptive name/id to be used per row or for the final single row
+	//(depending on axis)
+	{"label", required_argument, 0, 'l'},
+	{0,0,0,0}
+};
+
+
 //modified from https://stackoverflow.com/questions/3501338/c-read-file-line-by-line
 int main(int argc, char** argv)
 {
-	
+	int opt_;
+	int opt_idx = 0;
+
 	config_settings config_def;
 	config_def.chrm_col=0;
 	config_def.start_col=1;
 	config_def.end_col=2;
 	config_def.base_col_start=3;
-	
-	config_def.axis=0;
-	if(argc >= 2)
-		config_def.axis = atoi(argv[1]);
+	config_def.axis=COL_AXIS;
 	config_def.op = "sum";
-	if(argc >= 3)
-		config_def.op = argv[2];
+	config_def.label = "";
 
+	opt_ = getopt_long(argc, argv, "a:o:l:", long_opts, &opt_idx);
+	while(opt_ != -1)
+	{
+		switch(opt_)
+		{
+		case 'a':
+			config_def.axis = atoi(optarg);
+			break;
+		case 'o':
+			config_def.op = optarg;
+			break;
+		case 'l':
+			config_def.label = optarg;
+			break;
+		case '?':
+			break;
+		default:
+			exit(-1);
+		}
+		opt_idx = 0;
+		opt_ = getopt_long(argc, argv, "a:o:l:", long_opts, &opt_idx);
+	}
+	printf("axis: %d op: %s label: %s\n",config_def.axis,config_def.op,config_def.label);
+	
 	char* line = NULL;
 	size_t length = 0;
 	ssize_t bytes_read = 0;
-	bytes_read=getline(&line, &length, stdin);
-	double val = 0.0;
 	double* vals;
+	int num_vals = 0;
+	long num_rows = 0;
+	int first = 1;
+	bytes_read=getline(&line, &length, stdin);
+	//we only print coordinates if doing row-by-row (COL_AXIS) summaries
+	int print_coords_per_row = config_def.axis == COL_AXIS;
 	while(bytes_read != -1)
 	{
-		val = 0.0;
-		int num_toks = process_line(strdup(line), "\t", &val, config_def, 1);
-		if(strcmp(config_def.op, "mean") == 0)
-			fprintf(stdout,"\t%.3f\n",val);
-		else
-			fprintf(stdout,"\t%.0f\n",val);
-		fflush(stdout);
+		//assumes no header
+		num_rows++;
+		int num_toks = process_line(first, strdup(line), "\t", &vals, config_def, print_coords_per_row);
+		if(first)
+		{
+			num_vals = num_toks - config_def.base_col_start;
+			//one large calloc up front
+			vals = calloc(num_vals,sizeof(double));
+			first = 0;
+			num_toks = process_line(first, strdup(line), "\t", &vals, config_def, print_coords_per_row);
+		}
+		if(config_def.axis == COL_AXIS)
+		{
+			if(strcmp(config_def.op, "mean") == 0)
+				fprintf(stdout,"\t%.3f",vals[0]/num_vals);
+			else
+				fprintf(stdout,"\t%.0f",vals[0]);
+			printf("\n");
+			fflush(stdout);
+		}
+		//reset the value for a row computation
+		if(config_def.axis == COL_AXIS)
+			vals[0] = 0.0;
 		bytes_read=getline(&line, &length, stdin);
+	}
+	if(config_def.axis == ROW_AXIS)
+	{
+		if(strlen(config_def.label) > 0)
+			fprintf(stdout,"%s",config_def.label);
+		int j;
+		for(j=0; j < num_vals; j++)
+		{
+			if(strcmp(config_def.op, "mean") == 0)
+				fprintf(stdout,"\t%.3f",vals[j]/num_rows);
+			else
+				fprintf(stdout,"\t%.0f",vals[j]);
+		}
+		printf("\n");
+		fflush(stdout);
 	}
 	if(line)
 		free(line);
+	if(vals)
+		free(vals);
 }
 
