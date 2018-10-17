269e1fcaae010f5e4cd50515742b6dda51123b23
ChristopherWilks <broadsword@gmail.com>
2018-10-17 15:23:41 -0400

    fixed missing sample bug in lucene index builing script

--- deploy/build_lucene_indexes.sh
+++ deploy/build_lucene_indexes.sh
@@ -7,7 +7,7 @@ create_all=$1
 #also assumes we're in the directory where all the snaptron compilation data is at
 if [[ $create_all == "all" ]]; then
 	perl -e 'print "rail_id\tjunction_count\tjunction_coverage\tjunction_avg_coverage\n";' > jx_stats_per_sample.tsv
-	zcat junctions.bgz | cut -f 12 | perl -ne 'chomp; @f=split(/,/,$_); shift(@f); for $f (@f) { ($f,$c)=split(/:/,$f); $count{$f}++; $cov{$f}+=$c; } END { for $f (keys %count) { $avg=$cov{$f}/$count{$f}; print "$f\t".$count{$f}."\t".$cov{$f}."\t$avg\n";}}' | sort -t'	' -k1,1n >> jx_stats_per_sample.tsv
+	zcat junctions.bgz | cut -f 12 | perl -ne 'BEGIN { open(IN,"cut -f1 samples.tsv |"); while($line=<IN>) { next if($line!~/^\d+$/); chomp($line); $h{$line}=1; } close(IN); } chomp; @f=split(/,/,$_); shift(@f); for $f (@f) { ($f,$c)=split(/:/,$f); $count{$f}++; $cov{$f}+=$c; } END { for $f (keys %count) { $avg=$cov{$f}/$count{$f}; delete($h{$f}); print "$f\t".$count{$f}."\t".$cov{$f}."\t$avg\n";} for $f (keys %h) { print "$f\t0\t0\t0.0\n";} }' | sort -t'	' -k1,1n >> jx_stats_per_sample.tsv
 	paste samples.tsv <(cut -f 2- jx_stats_per_sample.tsv) > samples.jx.tsv
 	mv samples.jx.tsv samples.tsv
 fi
