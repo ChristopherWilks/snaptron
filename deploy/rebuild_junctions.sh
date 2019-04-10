38c333b64f0da1b9d0e66d9f3d288eef9366714f
ChristopherWilks <broadsword@gmail.com>
2019-04-10 15:53:14 -0400

    added tabix indexing, split out uncompressed; using named vars

--- deploy/rebuild_junctions.sh
+++ deploy/rebuild_junctions.sh
@@ -1,21 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o pipefail -o errexit 
 
 SQLITE3='sqlite3'
 BGZIP_COMP='bgzip'
+TABIX='tabix'
 BGZIP_UNCOMP='~/bgzip'
 
-#get this script's path
-p=`perl -e '@f=split("/","'$0'"); pop(@f); print "".join("/",@f)."\n";'`
+#e.g. "tcga"
+compilation=$1
+#e.g. junctions.gz, should be fully formatted already for Snaptron
+junctions=$2
+#might be undefined
+build_uncompressed_junctions=$3
 
-$SQLITE3 ${1}.sqlite < ${p}/snaptron_schema.sql
+scripts=$(dirname $0)
+
+if [[ -e ${compilation}.sqlite ]]; then
+    rm ${compilation}.sqlite
+fi
+$SQLITE3 ${compilation}.sqlite < ${scripts}/snaptron_schema.sql
 
 mkfifo ./import
 mkfifo ./import2
 
-zcat $2 | sort -k2,2 -k3,3n -k4,4n | tee ./import | tee ./import2 | $BGZIP_COMP > junctions.ordered.bgz &
-cat ./import2 | $BGZIP_UNCOMP > junctions_uncompressed.ordered.bgz
-$SQLITE3 ${1}.sqlite -cmd '.separator "\t"' ".import ./import intron"
-$SQLITE3 ${1}.sqlite < ${p}/snaptron_schema_index.sql
+zcat $junctions | sort -k2,2 -k3,3n -k4,4n | tee ./import | tee ./import2 | $BGZIP_COMP > junctions.ordered.bgz &
+if [[ -v $build_uncompressed_junctions ]]; then
+    cat ./import2 | $BGZIP_UNCOMP > junctions_uncompressed.ordered.bgz &
+else
+    cat ./import2 > /dev/null &
+fi
+$SQLITE3 ${compilation}.sqlite -cmd '.separator "\t"' ".import ./import intron"
+$SQLITE3 ${compilation}.sqlite < ${scripts}/snaptron_schema_index.sql
+
+tabix -0 -s 2 -b 3 -e 4 junctions.ordered.bgz
+if [[ -v $build_uncompressed_junctions ]]; then
+    tabix -0 -s 2 -b 3 -e 4 junctions_uncompressed.ordered.bgz
+fi
 
 rm ./import
 rm ./import2
