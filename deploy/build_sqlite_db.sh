86b0932953e7709e03453163395f56713767967b
ChristopherWilks <broadsword@gmail.com>
2019-08-16 12:30:35 -0400

    added run-specific suffix to sqlite db build

--- deploy/build_sqlite_db.sh
+++ deploy/build_sqlite_db.sh
@@ -5,9 +5,9 @@ p=`perl -e '@f=split("/","'$0'"); pop(@f); print "".join("/",@f)."\n";'`
 
 sqlite3 ${1}.sqlite < ${p}/snaptron_schema.sql
 
-mkfifo ./import
-zcat $2 > ./import  &
-sqlite3 ${1}.sqlite -cmd '.separator "\t"' ".import ./import intron"
+mkfifo ./import.${1}
+zcat $2 > ./import.${1}  &
+sqlite3 ${1}.sqlite -cmd '.separator "\t"' ".import ./import.${1} intron"
 
 sqlite3 ${1}.sqlite < ${p}/snaptron_schema_index.sql
-rm ./import
+rm ./import.${1}
