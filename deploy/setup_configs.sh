68e7a2a27179a91ea8cd4143b0beec78c00c0016
ChristopherWilks <broadsword@gmail.com>
2019-09-17 16:25:30 -0400

    handle srav2 and test nearly the same in config setup

--- deploy/setup_configs.sh
+++ deploy/setup_configs.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-ln -fs ./snaptron_server ./${1}_snaptron_server
-ln -fs instances/snapconf.py.${1} ./snapconf.py
-if [ $1 = "srav1" ] || [ $1 = "srav2" ] ; then
-	ln -f instances/test_snaptron.py.${1} ./test_snaptron.py
-	ln -fs instances/tests.sh.${1} ./tests.sh
-	ln -fs instances/test_s2i_ids_15.snaptron_ids.${1} ./test_s2i_ids_15.snaptron_ids
-	ln -fs instances/test_annot_full.${1} ./test_annot_full
-	ls instances/*ucsc*.${1} | perl -ne 'chomp; $s=$_; $s1=$s; $s1=~s/\.srav.$//; $s1=~s/instances\///; `ln -fs $s $s1`;'
+comp=$1
+
+ln -fs ./snaptron_server ./${comp}_snaptron_server
+ln -fs instances/snapconf.py.${comp} ./snapconf.py
+if [ $comp = "srav1" ] || [ $comp = "srav2" ] || [ $comp = "test" ]; then
+    comp2=$comp
+    if [ $comp = "test" ]; then
+        comp="srav2"
+    fi
+	ln -f instances/test_snaptron.py.${comp} ./test_snaptron.py
+	ln -fs instances/tests.sh.${comp} ./tests.sh
+	ln -fs instances/test_s2i_ids_15.snaptron_ids.${comp} ./test_s2i_ids_15.snaptron_ids
+	ln -fs instances/test_annot_full.${comp} ./test_annot_full
+	ls instances/*ucsc*.${comp} | perl -ne 'chomp; $s=$_; $s1=$s; $s1=~s/\.srav.$//; $s1=~s/instances\///; `ln -fs $s $s1`;'
+    comp=$comp2
 fi
-if [ $1 = "srav2" ] ; then
-	ln -fs instances/test_s2i_ids_3.snaptron_ids.${1} ./test_s2i_ids_3.snaptron_ids
-	ln -fs instances/lucene_range_ids.test.${1} ./lucene_range_ids.test
+if [ $comp = "srav2" ] || [ $comp = "test" ]; then
+    comp="srav2"
+	ln -fs instances/test_s2i_ids_3.snaptron_ids.${comp} ./test_s2i_ids_3.snaptron_ids
+	ln -fs instances/lucene_range_ids.test.${comp} ./lucene_range_ids.test
 fi
