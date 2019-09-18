e26b15252ef88d97af8035ad4fc5dc64d9c78231
ChristopherWilks <broadsword@gmail.com>
2019-09-17 20:40:07 -0400

    added additional apache2 mods needed for proxy; set debug on

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 DEPLOY_DIR="/deploy"
 
@@ -27,8 +28,14 @@ if [ "$1" == "deploy" ] ; then
    /bin/bash -x ${DEPLOY_DIR}/${compilation}/deploy/deploy_snaptron.sh ${compilation}
 # case 2): run (assume everything's already setup)
 elif [ "$1" == "run" ] ; then
-   ln -s /${DEPLOY_DIR}/${compilation}/instances/proxy.conf /etc/apache2/mods-enabled/
-   ln -s ../mods-available/proxy.load /etc/apache2/mods-enabled/
+   ln -fs /${DEPLOY_DIR}/${compilation}/instances/proxy.conf /etc/apache2/mods-available/
+   ln -fs /etc/apache2/mods-available/proxy.conf /etc/apache2/mods-enabled/
+   ln -fs ../mods-available/proxy.load /etc/apache2/mods-enabled/
+   ln -fs ../mods-available/slotmem_shm.load /etc/apache2/mods-enabled/
+   ln -fs ../mods-available/proxy_balancer.conf /etc/apache2/mods-enabled/
+   ln -fs ../mods-available/proxy_balancer.load /etc/apache2/mods-enabled/
+   ln -fs ../mods-available/lbmethod_byrequests.load /etc/apache2/mods-enabled/
+   ln -fs ../mods-available/proxy_http.load /etc/apache2/mods-enabled/
    /etc/init.d/apache2 start
    cd /${DEPLOY_DIR}/${compilation} && python /${DEPLOY_DIR}/${compilation}/snaptron_server --no-daemon
 else
