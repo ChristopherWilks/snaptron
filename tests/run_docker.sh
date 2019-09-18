93b2c0fc50e9f3069f3e1186ec3b1157ae1d681e
ChristopherWilks <broadsword@gmail.com>
2019-09-18 15:23:54 -0400

    one command to download from quay and run snaptron test container

--- tests/run_docker.sh
+++ tests/run_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Run previously built *test* Snaptron Docker image as a container
-
-docker run --rm -p 21656:1656 -p 2080:80 -i -t --name test_snaptron snaptron_tests
+dir=$(dirname $0)
+/bin/bash -x ${dir}/pull.sh
+docker run --rm -p 21656:1656 -p 2080:80 -i -t --name test_snaptron quay.io/broadsword/snaptron_tests
