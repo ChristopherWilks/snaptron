222248f0d651de0ef109af5a7e0d51e39d83934e
ChristopherWilks <broadsword@gmail.com>
2019-10-02 16:49:10 -0400

    added back in local port for srav2 testing on test docker

--- tests/run_docker.sh
+++ tests/run_docker.sh
@@ -2,4 +2,4 @@
 # Run previously built *test* Snaptron Docker image as a container
 dir=$(dirname $0)
 /bin/bash -x ${dir}/pull.sh
-docker run --rm -p 2080:80 -i -t --name test_snaptron quay.io/broadsword/snaptron_tests
+docker run --rm -p 21656:1656 -p 2080:80 -i -t --name test_snaptron quay.io/broadsword/snaptron_tests
