cee810ba68a74d7654dbb4f5ad15f9985d219142
ChristopherWilks <broadsword@gmail.com>
2020-09-17 12:01:46 -0400

    switch to pigz for sqlite builder

--- deploy/build_sqlite_db.sh
+++ deploy/build_sqlite_db.sh
@@ -6,7 +6,7 @@ p=`perl -e '@f=split("/","'$0'"); pop(@f); print "".join("/",@f)."\n";'`
 sqlite3 ${1}.sqlite < ${p}/snaptron_schema.sql
 
 mkfifo ./import.${1}
-zcat $2 > ./import.${1}  &
+pcat $2 > ./import.${1}  &
 sqlite3 ${1}.sqlite -cmd '.separator "\t"' ".import ./import.${1} intron"
 
 sqlite3 ${1}.sqlite < ${p}/snaptron_schema_index.sql
