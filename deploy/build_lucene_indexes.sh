12da20bdaba3141cbe97cb2aea5005628e3f6eba
ChristopherWilks <broadsword@gmail.com>
2020-09-17 12:01:15 -0400

    take samples file as a param in lucene index builder

--- deploy/build_lucene_indexes.sh
+++ deploy/build_lucene_indexes.sh
@@ -3,7 +3,8 @@ set -o pipefail -o errexit
 scripts=$(dirname $0)
 root=$scripts/../
 
-create_all=$1
+samples=$1
+create_all=$2
 #build Lucene metadata indices (assumes samples.tsv is present and is sorted by rail_id in ascending order)
 #also assumes we're in the directory where all the snaptron compilation data is at
 if [[ $create_all == "all" ]]; then
@@ -12,10 +13,11 @@ if [[ $create_all == "all" ]]; then
 	paste samples.tsv <(cut -f 2- jx_stats_per_sample.tsv) > samples.jx.tsv
 	mv samples.jx.tsv samples.tsv
 fi
-cat samples.tsv | perl $scripts/infer_sample_metadata_field_types.pl > samples.tsv.inferred
+cat $samples | perl $scripts/infer_sample_metadata_field_types.pl > samples.tsv.inferred
 if [ -d ./lucene_full_standard ]; then
 	rm -rf lucene_full_standard
 	rm -rf lucene_full_ws
 fi
-cat samples.tsv | python $scripts/../lucene_indexer.py samples.tsv.inferred ./ > lucene.indexer.run 2>&1
-head -1 samples.tsv | perl -ne 'BEGIN { print "index\tfield_name\n";} chomp; for $e (split(/\t/,$_)) { print "".$i++."\t$e\n";}' > samples.fields.tsv
+cat $samples | python $scripts/../lucene_indexer.py samples.tsv.inferred ./ > lucene.indexer.run 2>&1
+head -1 $samples | tr \\t \\n | fgrep -n "" | sed 's/:/\t/' > samples.fields.tsv
+#head -1 $samples | perl -ne 'BEGIN { print "index\tfield_name\n";} chomp; for $e (split(/\t/,$_)) { print "".$i++."\t$e\n";}' > samples.fields.tsv
