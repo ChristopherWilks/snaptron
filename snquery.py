6256d58a681fd8af84831ff2c639dd788da1eff0
ChristopherWilks <broadsword@gmail.com>
2021-05-25 19:10:41 -0400

    switching license to MIT

--- snquery.py
+++ snquery.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python2.7
 
-# Copyright 2016, Christopher Wilks <broadsword@gmail.com>
-#
 # This file is part of Snaptron.
 #
 # Snaptron is free software: you can redistribute it and/or modify
 # it under the terms of the 
-# Creative Commons Attribution-NonCommercial 4.0 
-# International Public License ("CC BY-NC 4.0").
 #
-# Snaptron is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# CC BY-NC 4.0 license for more details.
+#    The MIT License
 #
-# You should have received a copy of the CC BY-NC 4.0 license
-# along with Snaptron.  If not, see 
-# <https://creativecommons.org/licenses/by-nc/4.0/legalcode>.
+#    Copyright (c) 2016-  by Christopher Wilks <broadsword@gmail.com> 
+#                         and Ben Langmead <langmea@cs.jhu.edu>
+#
+#    Permission is hereby granted, free of charge, to any person obtaining
+#    a copy of this software and associated documentation files (the
+#    "Software"), to deal in the Software without restriction, including
+#    without limitation the rights to use, copy, modify, merge, publish,
+#    distribute, sublicense, and/or sell copies of the Software, and to
+#    permit persons to whom the Software is furnished to do so, subject to
+#    the following conditions:
+#
+#    The above copyright notice and this permission notice shall be
+#    included in all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+#    BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+#    ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+#    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
 
 import sys
 import re
