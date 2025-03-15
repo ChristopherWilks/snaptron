d21829fdc510121078c13c7f2a5e3c816a31554d
Christopher Wilks <cwilks3@jhu.edu>
2025-03-15 12:07:04 -0400

    add final reformatting script

---
+++ jxn2studies/reformat_jxn2studies.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+#chrom id (1,2,...,18,19,M,X,Y)
+c=$1
+pcat chr${c}.jxns2studies.gz | perl -ne 'chomp; $f=$_; ($j,$jid,$c,$s,$e,$o,$ns,$cov,$samples)=split(/\t/,$f,-1); print "$j\t$jid\t$c:$s-$e:$o\t$ns\t$cov\n"; @samps=split(/,/,$samples,-1); for $samp (@samps) { print "$j\t$jid\t$samp\n";}' | bgzip -@4 > chr${c}.jxn2studies.gz
+index -s2 -b1 -e1 chr${c}.jxn2studies.gz
